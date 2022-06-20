1279d767f547a0487773ced9da6db157eb05cab3
799189288@qq.com <799189288@qq.com>
2022-06-20 20:16:39 +0800

    bug

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld/>
+    <IntegralDetail/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-
+// import IntegralDetail from './components/IntegralDetail.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    // IntegralDetail
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,123 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <div class="tabBar">
+      <van-tabs v-model="active" @click="goto">
+        <van-tab
+          :title="i.title"
+          v-for="(i, index) in fake"
+          :key="index"
+        ></van-tab>
+      </van-tabs>
+    </div>
+    <div class="container">
+      <div
+        v-for="(i, index) in desti"
+        :key="index"
+        :id="'data' + index"
+        class="title"
+        ref="desti"
+      >
+        {{ i }}
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  name: "HelloWorld",
+  data() {
+    return {
+      active: 0,
+      desti: [],
+      fake: [
+        {
+          title: "1",
+          main: {
+            content: "这是一个内容",
+            time: "2019-01-01",
+          },
+        },
+        {
+          title: "2",
+          main: {
+            content: "这是一个内容",
+            time: "2019-01-01",
+          },
+        },
+        {
+          title: "3",
+          main: {
+            content: "这是一个内容",
+            time: "2019-01-01",
+          },
+        },
+        {
+          title: "4",
+          main: {
+            content: "这是一个内容",
+            time: "2019-01-01",
+          },
+        },
+        {
+          title: "5",
+          main: {
+            content: "这是一个内容",
+            time: "2019-01-01",
+          },
+        },
+        {
+          title: "6",
+          main: {
+            content: "这是一个内容",
+            time: "2019-01-01",
+          },
+        },
+        {
+          title: "7",
+          main: {
+            content: "这是一个内容",
+            time: "2019-01-01",
+          },
+        },
+        {
+          title: "8",
+          main: {
+            content: "这是一个内容",
+            time: "2019-01-01",
+          },
+        },
+      ],
+    };
+  },
+  methods: {
+    goto(name) {
+      this.$refs.desti[name].scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+      });
+    },
+  },
+  mounted() {
+    this.desti = this.fake.map((i) => i.title);
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
+<style scoped lang="scss">
+.container {
+  overflow: auto;
+  margin-top: 50px;
+  .title {
+    height: 500px;
+    background: #f5f5f5;
+    text-align: center;
+  }
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.tabBar {
+  position: fixed;
+  width: 100%;
+  top: 0;
 }
 </style>
